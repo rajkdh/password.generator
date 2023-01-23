dc0ddb7b5899c43476bf290ef3013b53079da925
Rajvir Kaur Dhanoa <rajd@Rajvirs-MBP.ht.home>
2023-01-23 14:51:38 -0500

    added variables and assigned value for password generator, number, symbol, lowercase, and uppercase.

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,8 @@ function generatePassword(){
     var userWantsSymbols = window.confirm('include symbols in your password?')
     var userWantsLowerCase = window.confirm('include lowercase letters in your password?')
     var userWantsUpperCase = window.confirm('include uppercase letters in your password?')
+
+    var numberList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    var symbolList = ['!', '@', '#', '$', '%', '^', '&', '*']
+    var lowercaseList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    var uppercaseList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
