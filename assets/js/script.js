4e32223378170a04788d19c3aeaeec7016fe725e
Rajvir Kaur Dhanoa <rajd@Rajvirs-MBP.ht.home>
2023-01-23 15:04:24 -0500

    added function to add confirmed window prompts to lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,17 @@ function generatePassword(){
     var symbolList = ['!', '@', '#', '$', '%', '^', '&', '*']
     var lowercaseList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     var uppercaseList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    var optionList = []
+
+    if (userWantsNumbers === true){
+        optionList.push(numberList)
+    } if (userWantsSymbols === true){
+        optionList.push(symbolList)
+    } if (userWantsLowerCase === true){
+        optionList.push(lowercaseList)
+    } if (userWantsUpperCase === true){
+        optionList.push(uppercaseList)
+    } if (optionList.length === 0){
+        optionList.push(lowercaseList)
+    }
